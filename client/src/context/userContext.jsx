43c4f5a24c95f7b7bf8df7359cfb28e8fd28851a
Raunak Gupta <raunakgupta9755@gmail.com>
2025-09-19 11:17:52 +0530

    Cloudinary + multer set up

--- client/src/context/userContext.jsx
+++ client/src/context/userContext.jsx
@@ -1,4 +1,5 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
 
 export const userDataContext = createContext();
 
@@ -8,6 +9,23 @@ const UserContext = ({ children }) => {
     serverUrl,
   };
 
+  const [userData, setUserData] = useState(null);
+
+  const handleCurrentUser = async () => {
+    try {
+      const result = await axios.get(`${serverUrl}/api/user/currentuser`, {
+        withCredentials: true,
+      });
+      setUserData(result.data);
+      console.log(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handleCurrentUser();
+  }, []);
   return (
     <>
       <div>
